1ff60bad2ea7bf06fda373a33951fd8a3e0bd2ed
Gustavo Guerreiro <gustavogbc@gmail.com>
2024-09-12 07:04:25 -0300

    incluindo docker para testes

--- arvore_avl.py
+++ arvore_avl.py
@@ -4,3 +4,16 @@ class No:
         self.direita = None
         self.altura = 0
 
+class Arvore:
+    def __init__(self) -> None:
+        pass
+
+    def inserir(A, n):
+        return 0
+    
+    def calcularDiferenca(A):
+        #calcular diferenca das alturas das subarvores esquerda e direita
+        #calculaAlturaAesq = hEsq
+        #calcularAlturaAdir = hDir
+        #dif = hDir - hEsq
+        pass
